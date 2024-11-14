d7086da9761c69bc730256edc3df659c5c105e6c
Seo-Yurim <yurim2204@naver.com>
2024-11-14 18:48:27 +0900

    feat: 초기 세팅

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={styles.home}>
+        이 페이지는 저희 프로젝트의
+        <br />
+        <span>랜딩 페이지</span>가 될 예정이에요 &gt;_0
+      </h1>
+    </>
+  );
+}
