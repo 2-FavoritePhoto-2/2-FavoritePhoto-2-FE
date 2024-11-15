4d71777ab2583db6f482b8c6bef20dc2b62d9df9
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-15 15:40:28 +0900

    refactor(서유림): px을 rem으로 변경

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Rating from "@/components/Common/Dropdown/Sort/Rating";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
+      <Rating />
       <h1 className={styles.home}>
         이 페이지는 저희 프로젝트의
         <br />
