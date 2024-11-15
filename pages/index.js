9b22ccaa1651b9ebfe0a963dab17687064a206ba
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-15 13:06:31 +0900

    feat: 검색 컴포넌트

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import styles from '@/styles/Home.module.css';
+import SearchBar from "@/components/Common/SearchBar/SearchBar";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <br />
         <span>랜딩 페이지</span>가 될 예정이에요 &gt;_0
       </h1>
+      <SearchBar />
     </>
   );
 }
