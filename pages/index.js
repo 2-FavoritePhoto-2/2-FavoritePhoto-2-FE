2e25c12a46fcd0053d6ecf0dddaa07117c9f980b
gahyeon-yang <didrkgus0426@gmail.com>
2024-11-15 15:42:00 +0900

    fix: index.js 코드 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Rating from "@/components/Common/Dropdown/Sort/Rating";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Rating />
       <h1 className={styles.home}>
         이 페이지는 저희 프로젝트의
         <br />
