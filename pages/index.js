67ce55ee50d5104219e3c1b591533fd8e7310f8b
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-18 14:13:25 +0900

    fix: index에서 컴포넌트 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import SuccessFail from "@/components/Common/SuccessFail/SuccessFail";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <SuccessFail type="register_success" />
       <h1 className={styles.home}>
         이 페이지는 저희 프로젝트의
         <br />
