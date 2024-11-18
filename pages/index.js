fc725d71d71e3bef3190d8b9cda312f8714f7fca
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-18 14:11:44 +0900

    feat: 성공 실패 모달 ui 및 미디어쿼리

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import SuccessFail from "@/components/Common/SuccessFail/SuccessFail";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
+      <SuccessFail type="register_success" />
       <h1 className={styles.home}>
         이 페이지는 저희 프로젝트의
         <br />
