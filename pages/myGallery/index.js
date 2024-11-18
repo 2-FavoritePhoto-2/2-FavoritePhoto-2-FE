f09a3c9b56806c89e1b1b3f694a28c9c672c512c
Seo-Yurim <yurim2204@naver.com>
2024-11-18 18:20:56 +0900

    feat: 보유 카드 섹션 UI 구현

--- pages/myGallery/index.js
+++ pages/myGallery/index.js
@@ -1,10 +1,12 @@
 import styles from "@/styles/MyGallery.module.css";
 import MyGalleryTitle from "@/components/MyGallery/MyGalleryTitle";
+import MyOwnedCards from "@/components/MyGallery/MyOwnedCards";
 
 export default function MyGallery() {
   return (
     <div>
       <MyGalleryTitle />
+      <MyOwnedCards />
     </div>
   );
 }
