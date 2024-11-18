ecdaf23377c0838a263ca6c032a6bd267ac4de29
Seo-Yurim <yurim2204@naver.com>
2024-11-18 18:28:30 +0900

    feat: 검색, 필터 섹션  UI 구현

--- pages/myGallery/index.js
+++ pages/myGallery/index.js
@@ -1,12 +1,14 @@
 import styles from "@/styles/MyGallery.module.css";
 import MyGalleryTitle from "@/components/MyGallery/MyGalleryTitle";
 import MyOwnedCards from "@/components/MyGallery/MyOwnedCards";
+import MyGalleryFilter from "@/components/MyGallery/MyGalleryFilter";
 
 export default function MyGallery() {
   return (
-    <div>
+    <div className={styles.header}>
       <MyGalleryTitle />
       <MyOwnedCards />
+      <MyGalleryFilter />
     </div>
   );
 }
