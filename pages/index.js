b8a837e0c1784e731fdeeaf462a8e49f847bc6ad
Seo-Yurim <yurim2204@naver.com>
2024-11-18 17:31:06 +0900

    test: 테스트 코드 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import styles from "@/styles/Home.module.css";
-import CardSell from "@/components/Common/Modal/CardSell";
 
 export default function Home() {
   return (
     <>
-      <CardSell />
       <h1 className={styles.home}>
         이 페이지는 저희 프로젝트의
         <br />
