5ff46421f339d105f4fe85f11562863df544e5c4
Seo-Yurim <yurim2204@naver.com>
2024-11-18 18:41:59 +0900

    feat: 카드 리스트 UI 및 전체 레이아웃 구현

--- pages/myGallery/index.js
+++ pages/myGallery/index.js
@@ -2,13 +2,17 @@ import styles from "@/styles/MyGallery.module.css";
 import MyGalleryTitle from "@/components/MyGallery/MyGalleryTitle";
 import MyOwnedCards from "@/components/MyGallery/MyOwnedCards";
 import MyGalleryFilter from "@/components/MyGallery/MyGalleryFilter";
+import MyGalleryList from "@/components/MyGallery/MyGalleryList";
 
 export default function MyGallery() {
   return (
-    <div className={styles.header}>
-      <MyGalleryTitle />
-      <MyOwnedCards />
-      <MyGalleryFilter />
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <MyGalleryTitle />
+        <MyOwnedCards />
+        <MyGalleryFilter />
+      </div>
+      <MyGalleryList />
     </div>
   );
 }
