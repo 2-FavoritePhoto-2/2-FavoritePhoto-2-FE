f368248886e28b726178012c2dbc7ea49922dbf6
Seo-Yurim <yurim2204@naver.com>
2024-11-19 17:04:31 +0900

    design: 마이갤러리 페이지 미디어쿼리

--- pages/myGallery/index.js
+++ pages/myGallery/index.js
@@ -3,12 +3,10 @@ import MyGalleryTitle from "@/components/MyGallery/MyGalleryTitle";
 import MyOwnedCards from "@/components/MyGallery/MyOwnedCards";
 import MyGalleryFilter from "@/components/MyGallery/MyGalleryFilter";
 import MyGalleryList from "@/components/MyGallery/MyGalleryList";
-import CardList from "@/components/Common/Modal/CardList";
 
 export default function MyGallery() {
   return (
     <div className={styles.container}>
-      <CardList />
       <div className={styles.header}>
         <MyGalleryTitle />
         <MyOwnedCards />
