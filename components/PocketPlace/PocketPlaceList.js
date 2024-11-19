8fa574bc3c6c6c4207e1fffae5bcb20a38314aa9
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-19 13:29:01 +0900

    feat: 무한 스크롤링 주석처리

--- components/PocketPlace/PocketPlaceList.js
+++ components/PocketPlace/PocketPlaceList.js
@@ -3,8 +3,29 @@ import PhotoCard from "@/components/PhotoCard/PhotoCard";
 import { useState, useEffect } from "react";
 
 export default function MyGalleryList() {
-  const count = 30;
+  const initialCount = 40;
   const [cardPerRow, setCardPerRow] = useState(3);
+  //   const [count, setCount] = useState(initialCount);
+
+  /*TODO
+   * 1. 창크기별로 보이는 ROW 개수 해야하나
+   * 2. 카드 미디어쿼리 완성 후 페이지 미디어쿼리 진행하기
+   */
+
+  //   const handleScroll = () => {
+  // const scrollPosition = window.innerHeight + document.documentElement.scrollTop;
+  // const bottomPosition = document.documentElement.offsetHeight;
+  //
+  // if (scrollPosition >= bottomPosition - 200) {
+  //   loadMoreCards();
+  // }
+  //   };
+
+  //   const loadMoreCards = () => {
+  // if (count < 100) {
+  //   setCount(count + 20);
+  // }
+  //   };
 
   useEffect(() => {
     const updateCardPerRow = () => {
@@ -14,16 +35,17 @@ export default function MyGalleryList() {
         setCardPerRow(3);
       }
     };
-
     updateCardPerRow();
     window.addEventListener("resize", updateCardPerRow);
+    // window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("resize", updateCardPerRow);
+      //   window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const rows = Array.from({ length: Math.ceil(count / cardPerRow) });
+  const rows = Array.from({ length: Math.ceil(initialCount / cardPerRow) });
 
   return (
     <div className={styles.pocketItem_container}>
