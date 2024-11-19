cb3586db840a29423d55f77b73e0dc50a24e76d6
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-19 13:31:34 +0900

    fix: 버튼 텍스트 수정

--- components/PocketPlace/PocketPlaceTitle.js
+++ components/PocketPlace/PocketPlaceTitle.js
@@ -5,7 +5,7 @@ export default function PocketPlaceTitle() {
     <div className={styles.container}>
       <div className={styles.title_button}>
         <h1>포켓플레이스</h1>
-        <button>포토카드 생성하기</button>
+        <button>나의 포토카드 판매하기</button>
       </div>
       <div className={styles.line}></div>
     </div>
