cde75c3ae475df11f32e1171b53fe6bdb40b7a37
Seo-Yurim <yurim2204@naver.com>
2024-11-20 17:42:40 +0900

    feat: 마이갤러리 포토카드 생성 누르면 생성하기 페이지로 이동

--- components/MyGallery/MyGalleryTitle.js
+++ components/MyGallery/MyGalleryTitle.js
@@ -1,11 +1,14 @@
 import styles from "./MyGalleryTitle.module.css";
+import { useRouter } from "next/router";
 
 export default function MyGalleryTitle() {
+  const router = useRouter;
+
   return (
     <div className={styles.container}>
       <div className={styles.title_button}>
         <h1>마이 갤러리</h1>
-        <button>포토카드 생성하기</button>
+        <button onClick={() => router.push("/myGallery/create")}>포토카드 생성하기</button>
       </div>
       <div className={styles.line}></div>
     </div>
