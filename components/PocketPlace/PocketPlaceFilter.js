ec484d6b0378889183fab8a56daf7f802b76302a
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-20 16:00:33 +0900

    feat: 모바일 미디어쿼리 적용

--- components/PocketPlace/PocketPlaceFilter.js
+++ components/PocketPlace/PocketPlaceFilter.js
@@ -1,19 +1,24 @@
 import styles from "./PocketPlaceFilter.module.css";
+import Image from "next/image";
 import SearchBar from "../Common/SearchBar/SearchBar";
 import Rating from "../Common/Dropdown/Sort/Rating";
 import Attribute from "../Common/Dropdown/Sort/Attribute";
 import Soldout from "../Common/Dropdown/Sort/Soldout";
 import Sort from "../Common/Dropdown/Sort/Sort";
 import MultiFilterModal from "../Common/Modal/MultiFilter";
+import icon_exchange from "@/public/assets/icon_exchange.svg";
 
 export default function PocketPlaceFilter() {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.search_filters}>
-          <div className={styles.filters}>
+          <div className={styles.searchBar_container}>
             <SearchBar />
-            <div className={styles.desktopOnly}>
+          </div>
+          <div className={styles.line}></div>
+          <div className={styles.filters}>
+            <div className={`${styles.desktopOnly} ${styles.rating}`}>
               <Rating />
             </div>
             <div className={styles.desktopOnly}>
@@ -22,12 +27,17 @@ export default function PocketPlaceFilter() {
             <div className={styles.desktopOnly}>
               <Soldout />
             </div>
+          </div>
+          <div className={styles.desktopOnly}>
+            <Image src={icon_exchange} alt="새로고침" width={20} className={styles.refreshIcon} />
+          </div>
+          <div className={styles.filters_mobile}>
             <div className={styles.mobileOnly}>
               <MultiFilterModal filterKeys={["등급", "속성", "매진여부"]} />
             </div>
-          </div>
-          <div className={styles.sort}>
-            <Sort />
+            <div className={styles.sort}>
+              <Sort />
+            </div>
           </div>
         </div>
       </div>
