f71253b0dad8c100aa57a9e6492b44fff8f7a4ae
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-20 13:16:20 +0900

    feat: 임시 랜딩페이지

---
+++ components/Landing/Landing.js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import styles from "./Landing.module.css";
+import pick from "@/public/assets/pick.gif";
+
+export default function Landing() {
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    router.push("/pocket_place"); // 임시로 이동
+  };
+
+  return (
+    <>
+      <div className={styles.home_container}>
+        <h1 className={styles.home}>
+          <span>피카</span>
+          <div className={styles.pick_wrapper}>
+            <Image src={pick} alt="로고이미지" layout="responsive" width={200} height={200} />
+          </div>
+          <span>포토</span>
+        </h1>
+        <button className={styles.home_button} onClick={handleButtonClick}>
+          로그인하러가기
+        </button>
+      </div>
+    </>
+  );
+}
--- components/PocketPlace/PocketPlaceFilter.js
+++ components/PocketPlace/PocketPlaceFilter.js
@@ -21,8 +21,6 @@ export default function PocketPlaceFilter() {
           </div>
         </div>
       </div>
-
-      {/* </div> */}
     </>
   );
 }
