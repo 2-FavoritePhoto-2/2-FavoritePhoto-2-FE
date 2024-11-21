02f39d984380bdd6ab633ce4ab68c576548c04a0
hello-yoon <meowyooon@gmail.com>
2024-11-21 17:18:18 +0900

    [김윤서] feat:app파일 gnb 연결

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '@/styles/globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Head from 'next/head';
 import GlobalNavigationBar from '@/lib/gnb/gnb';
+import Head from 'next/head';
+
 
 const noto = Noto_Sans_KR({ subsets: ['latin'] });
 
