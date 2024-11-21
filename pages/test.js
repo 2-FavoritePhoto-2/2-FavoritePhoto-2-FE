05f0d31d71955f184c42cf41777762f9fa14cd76
hello-yoon <meowyooon@gmail.com>
2024-11-21 16:36:10 +0900

    [김윤서] feat:gnb css 수정, props 수정

--- pages/test.js
+++ pages/test.js
@@ -1,27 +1,32 @@
 import Gnb from '../lib/gnb/gnb.js';
+import { useState } from "react";
 
 export default function TestPage() {
-  
   // 테스트용 데이터 true false
-  const isLoggedIn = true; // 로그인 여부
+  const [isLoggedIn , setIsLoggedIn ] = useState(true); // 로그인 여부
   const points = 1540; // 보유 포인트
   const nickname = '유디'; // 닉네임
 
 
-  const handleLogout = () => {
-    console.log('로그아웃되었습니다.');
+  const handleAuthChange = () => {
+    // 로그인/로그아웃 상태 전환
+    if ( isLoggedin ) {
+      console.log("로그아웃되었습니다.");
+      setIsLoggedIn (false); 
+    } else {
+      console.log("로그인되었습니다.");
+      setIsLoggedIn (true);
+    }
   };
 
   return (
   
     <>
-      <Gnb isLoggedIn={isLoggedIn} points={points} nickname={nickname} onLogout={handleLogout} />
+      <Gnb isLoggedIn={isLoggedIn} points={points} nickname={nickname} handleAuthChange={handleAuthChange} />
 
-      <main style={{ padding: '20px' }}>
+      <main style={{ padding: '2rem' }}>
         <h1>GNB 테스트 페이지</h1>
-        <p>이 페이지는 GNB 테스트를 위해 만들어졌습니다.
-
-        </p>
+        <p>이 페이지는 GNB 테스트를 위해 만들어졌습니다.</p>
       </main>
     </>
   );
