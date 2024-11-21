27d77d2820e06ec7f5c782a55b189db189196b08
Seo-Yurim <yurim2204@naver.com>
2024-11-21 11:27:27 +0900

    feat: 포토카드 데이터 연결

--- components/Common/Modal/CardList.js
+++ components/Common/Modal/CardList.js
@@ -4,8 +4,9 @@ import Rating from "../Dropdown/Sort/Rating";
 import Attribute from "../Dropdown/Sort/Attribute";
 import PhotoCard from "@/components/Common/PhotoCard/PhotoCard";
 import MultiFilterModal from "./MultiFilter";
+import data from "@/public/mockData.json";
 
-export default function CardList({ cardList }) {
+export default function CardList() {
   const count = 10;
   return (
     <div className={styles.list_wrapper}>
@@ -30,15 +31,10 @@ export default function CardList({ cardList }) {
       </div>
       <div className={styles.card_list_wrapper}>
         <div className={styles.card_list}>
-          {Array.from({ length: count }).map((_, index) => (
-            <PhotoCard key={index} />
+          {data.map((card) => (
+            <PhotoCard key={card.id} card={card} />
           ))}
         </div>
-        {/* {cardList.map((card) => (
-            <div className={styles.card_list} key={card.id}>
-              <PhotoCard card={card} />
-            </div>
-          ))} */}
       </div>
     </div>
   );
