23ec37e8b5711b334a5fd0be4836ad0531392ab9
Seo-Yurim <yurim2204@naver.com>
2024-11-21 15:33:58 +0900

    test: test 코드 삭제

--- components/Common/CardInfo/MyCardDetail.js
+++ components/Common/CardInfo/MyCardDetail.js
@@ -3,7 +3,6 @@ import styles from "./MyCardDetail.module.css";
 import Grade from "../Grade/Grade";
 import Modal from "../Modal/Modal";
 import CardList from "@/components/Common/Modal/CardList";
-import CardSell from "../Modal/CardSell";
 
 export default function MyCardDetail({ data }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -56,7 +55,7 @@ export default function MyCardDetail({ data }) {
       </button>
       {isOpen && (
         <Modal isOpen={isOpen} closeModal={handleClose}>
-          <CardSell />
+          <CardList />
         </Modal>
       )}
     </div>
