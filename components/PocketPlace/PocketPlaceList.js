f6fd1c772e358c450d7a56743e9ba30de4859fc4
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-22 17:48:24 +0900

    feat: 데이터 연결

--- components/PocketPlace/PocketPlaceList.js
+++ components/PocketPlace/PocketPlaceList.js
@@ -2,10 +2,11 @@ import styles from "./PocketPlaceList.module.css";
 import PhotoCard from "../Common/PhotoCard/PhotoCard";
 import { useState, useEffect } from "react";
 import throttle from "lodash/throttle";
+import { getCards } from "@/lib/api/pocketPlaceAPI";
 
 export default function PocketPlaceList() {
   const [cardPerRow, setCardPerRow] = useState(3);
-  const [mockData, setMockData] = useState([]);
+  const [cardItems, setCardItems] = useState([]);
   const [count, setCount] = useState(12);
   const [loading, setLoading] = useState(false);
 
@@ -16,15 +17,15 @@ export default function PocketPlaceList() {
   const handleScroll = throttle(() => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    if (mockData.length === 0 || scrollTop + clientHeight < scrollHeight - 200 || loading) return;
+    if (cardItems.length === 0 || scrollTop + clientHeight < scrollHeight - 200 || loading) return;
 
-    if (count >= mockData.length) return;
+    if (count >= cardItems.length) return;
 
     setLoading(true);
     try {
-      const newCards = mockData.slice(count, count + 12);
-      setMockData((prevData) => [...prevData, ...newCards]);
-      setCount((prevCount) => Math.min(prevCount + 12, mockData.length));
+      const newCards = cardItems.slice(count, count + 12);
+      setCardItems((prevData) => [...prevData, ...newCards]);
+      setCount((prevCount) => Math.min(prevCount + 12, cardItems.length));
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -44,17 +45,15 @@ export default function PocketPlaceList() {
     };
   }, [handleScroll]);
 
-  //TODO: 실제 API 연결 부분
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/mockData.json");
-        if (response.ok) {
-          const data = await response.json();
-          setMockData(data);
-        }
+        const data = await getCards("/shop");
+
+        const cards = data.list.map((item) => item.card);
+        setCardItems(cards);
       } catch (error) {
-        console.error(error);
+        console.error("패치 실패", error);
       }
     };
     fetchData();
@@ -66,11 +65,11 @@ export default function PocketPlaceList() {
     <div className={styles.pocketItem_container}>
       {rows.map((_, rowIndex) => (
         <div className={styles.row} key={rowIndex}>
-          {mockData.slice(rowIndex * cardPerRow, rowIndex * cardPerRow + cardPerRow).map((item) => {
-            const updatedItem = { ...item };
-
-            return <PhotoCard key={item.id} card={updatedItem} />;
-          })}
+          {cardItems
+            .slice(rowIndex * cardPerRow, rowIndex * cardPerRow + cardPerRow)
+            .map((item) => {
+              return <PhotoCard key={item.id} card={item} />;
+            })}
         </div>
       ))}
     </div>
