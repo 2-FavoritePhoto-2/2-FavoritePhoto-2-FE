3b5e73443bb6b1ffb9c385b8648116b9ea90b17f
yoonseo <meowyooon@gmail.com>
2024-11-25 04:42:12 +0900

    feat: gnb 수정 _app.js 연결

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,22 @@ import { Noto_Sans_KR } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalNavigationBar from '@/lib/gnb/gnb';
 import Head from 'next/head';
+import { useState } from 'react';
 
 const noto = Noto_Sans_KR({ subsets: ['latin'] });
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 관리
+  const points = 1540; // 보유 포인트
+  const nickname = "유디"; // 닉네임
+
+  // 인증 상태 변경 함수
+  const handleAuthChange = () => {
+    setIsLoggedIn((prev) => !prev); // 로그인 상태 반전
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
@@ -17,9 +27,16 @@ export default function App({ Component, pageProps }) {
       </Head>
       
       <div className={noto.className}>
-        <GlobalNavigationBar />
+        <GlobalNavigationBar 
+        isLoggedin={isLoggedIn} 
+        setIsLoggedIn={setIsLoggedIn} 
+        points={points} 
+        nickname={nickname} 
+        handleAuthChange={handleAuthChange} 
+        />
         <Component {...pageProps} />
       </div>
     </QueryClientProvider>
   );
 }
+
