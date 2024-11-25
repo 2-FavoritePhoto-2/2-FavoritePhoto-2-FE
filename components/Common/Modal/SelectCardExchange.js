a68fac5681223691764cbfc08a751d9f2520479d
bangsinchur <sinchurbang123@gmail.com>
2024-11-25 19:30:40 +0900

    feat: 상세페이지 api 연결

--- components/Common/Modal/SelectCardExchange.js
+++ components/Common/Modal/SelectCardExchange.js
@@ -60,7 +60,7 @@ export default function SelectCardExchange({ data, onClose }) {
             <p className={styles.exchange_name}>{data.card.name}</p>
             <div className={styles.photocard_content}>
               <div className={styles.photocard_size}>
-                <PhotoCard data={data ?? {}} />
+                <PhotoCard data={data ?? {}} exchange={true} />
               </div>
               <div className={styles.select_table}>
                 <div className={styles.input_size}>
