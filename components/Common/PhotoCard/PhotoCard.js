cb22a89905febf5134ec05a91e50f1c3eaff9c73
Seo-Yurim <yurim2204@naver.com>
2024-11-25 19:11:45 +0900

    fix: 데이터 변경

--- components/Common/PhotoCard/PhotoCard.js
+++ components/Common/PhotoCard/PhotoCard.js
@@ -9,20 +9,20 @@ export default function PhotoCard({ data, type = "판매카드", exchange }) {
     type === "내카드" ? (
       <>
         <div className={styles.img_wrap}>
-          <Image className={styles.img} src={data.image} fill alt="카드 이미지" priority />
+          <Image className={styles.img} src={data.card.image} fill alt="카드 이미지" priority />
         </div>
         <div className={styles.card_info}>
           <div className={styles.card_header}>
-            <h1>{data.name}</h1>
+            <h1>{data.card.name}</h1>
             <div className={styles.meta_info}>
               <div className={styles.category}>
                 <Grade grade={data.card.grade} />
                 <p className={styles.vert_line}>|</p>
-                <p className={styles.type}>{data.type[0]}</p>
-                {data.type[1] ? (
+                <p className={styles.type}>{data.card.type[0]}</p>
+                {data.card.type[1] ? (
                   <>
                     <p className={styles.vert_line}>|</p>
-                    <p>{data.type[1]}</p>
+                    <p>{data.card.type[1]}</p>
                   </>
                 ) : (
                   ""
@@ -36,11 +36,11 @@ export default function PhotoCard({ data, type = "판매카드", exchange }) {
           <div className={styles.card_price_quantity}>
             <div className={styles.card_price}>
               <p className={styles.label}>가격</p>
-              <p className={styles.point}>{data.price}p</p>
+              <p className={styles.point}>{data.card.price}p</p>
             </div>
             <div className={styles.card_quantity}>
               <p className={styles.label}>수량</p>
-              <p className={styles.value}>{data.remainingQuantity}</p>
+              <p className={styles.value}>{data.card.remainingQuantity}</p>
             </div>
           </div>
         </div>
