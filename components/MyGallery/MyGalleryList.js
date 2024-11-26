84900e0eb812344f74db31487d971a28eb375a6e
Seo-Yurim <yurim2204@naver.com>
2024-11-26 15:02:08 +0900

    feat: 마이갤러리 api 연결

--- components/MyGallery/MyGalleryList.js
+++ components/MyGallery/MyGalleryList.js
@@ -1,13 +1,12 @@
 import styles from "./MyGalleryList.module.css";
 import PhotoCard from "../Common/PhotoCard/PhotoCard";
-import data from "@/public/mockData.json";
 
-export default function MyGalleryList() {
+export default function MyGalleryList({ myCardList = [] }) {
   return (
     <div className={styles.card_list}>
-      {data.map((card) => (
+      {myCardList.map((card) => (
         <div key={card.id}>
-          <PhotoCard card={card} />
+          <PhotoCard data={card} type="내카드" />
         </div>
       ))}
     </div>
