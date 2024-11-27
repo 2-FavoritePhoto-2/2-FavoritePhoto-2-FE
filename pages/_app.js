719918d2253abd3777564e89b873f7cd9bbb39ca
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-27 14:38:19 +0900

    feat: 바디 마진 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import '@/styles/globals.css';
-import { Noto_Sans_KR } from 'next/font/google';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import GlobalNavigationBar from '@/lib/gnb/gnb';
-import Head from 'next/head';
-import { useState } from 'react';
+import "@/styles/globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GlobalNavigationBar from "@/lib/gnb/gnb";
+import Head from "next/head";
+import { useState } from "react";
 
-const noto = Noto_Sans_KR({ subsets: ['latin'] });
+const noto = Noto_Sans_KR({ subsets: ["latin"] });
 
 const queryClient = new QueryClient();
 
@@ -25,16 +25,18 @@ export default function App({ Component, pageProps }) {
         <title>피카픽포토</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className={noto.className}>
-        <GlobalNavigationBar 
-          isLoggedin={isLoggedIn} 
-          setIsLoggedIn={setIsLoggedIn} 
-          points={points} 
-          nickname={nickname} 
-          handleAuthChange={handleAuthChange} 
+        <GlobalNavigationBar
+          isLoggedin={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          points={points}
+          nickname={nickname}
+          handleAuthChange={handleAuthChange}
         />
-        <Component {...pageProps} />
+        <div className="body_container">
+          <Component {...pageProps} />
+        </div>
       </div>
     </QueryClientProvider>
   );
