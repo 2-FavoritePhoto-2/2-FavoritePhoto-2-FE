b26ebb7c9db3ca0bcc94632bcb30ca9eceb31e33
Seo-Yurim <yurim2204@naver.com>
2024-11-28 11:31:25 +0900

    design: 마이갤러리 페이지 모바일 쿼리 수정

--- components/MyGallery/MyGalleryTitle.js
+++ components/MyGallery/MyGalleryTitle.js
@@ -1,4 +1,7 @@
 import styles from "./MyGalleryTitle.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import back from "@/public/assets/icon_back.svg";
 import { useRouter } from "next/router";
 
 export default function MyGalleryTitle() {
@@ -7,6 +10,11 @@ export default function MyGalleryTitle() {
   return (
     <div className={styles.container}>
       <div className={styles.title_button}>
+        <div className={styles.back_icon}>
+          <Link href="/pocketPlace">
+            <Image src={back} width={22} height={22} alt="뒤로가기 아이콘" />
+          </Link>
+        </div>
         <h1>마이 갤러리</h1>
         <button onClick={() => router.push("/myGallery/create")}>포토카드 생성하기</button>
       </div>
