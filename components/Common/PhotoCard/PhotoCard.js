575dff4f0a5b9d61e2688e50a723e85c05191a0e
Seo-Yurim <yurim2204@naver.com>
2024-11-29 15:38:38 +0900

    feat: 배지 추가

--- components/Common/PhotoCard/PhotoCard.js
+++ components/Common/PhotoCard/PhotoCard.js
@@ -2,8 +2,25 @@ import styles from "./PhotoCard.module.css";
 import Image from "next/image";
 import logo from "@/public/assets/logo.svg";
 import Grade from "../Grade/Grade";
+import { useState, useEffect } from "react";
+import { getUserProfile } from "@/lib/api/UserService";
 
 export default function PhotoCard({ data, type = "판매카드", exchange }) {
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchProfile = await getUserProfile();
+        setProfile(fetchProfile);
+      } catch (err) {
+        console.error("프로필 정보를 불러오는데 실패했습니다.", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const exchangeHeaderClass = exchange ? styles.exchange_header : "";
   const exchangeClass = exchange ? styles.exchange : "";
   const exchangeValueClass = exchange ? styles.exchange_value : "";
@@ -11,7 +28,52 @@ export default function PhotoCard({ data, type = "판매카드", exchange }) {
   const exchangeLogoClass = exchange ? styles.exchange_logo : "";
 
   const cardType =
-    type === "내카드" ? (
+    type === "내판매카드" ? (
+      <>
+        <div className={`${styles.img_wrap}`}>
+          <div
+            className={`${styles.badge} ${
+              data.mode === "shop" ? styles.shop_badge : styles.exchange_badge
+            }`}
+          >
+            <p>{data.mode === "shop" ? "판매 중" : "교환 제시 대기 중"}</p>
+          </div>
+          <Image className={styles.img} src={data.image} fill alt="카드 이미지" priority />
+        </div>
+        <div className={styles.card_info}>
+          <div className={styles.card_header}>
+            <h1>{data.name}</h1>
+            <div className={styles.meta_info}>
+              <div className={styles.category}>
+                <Grade grade={data.grade} />
+                <p className={`${styles.vert_line}`}>|</p>
+                <p className={`${styles.type}`}>{data.type[0]}</p>
+                {data.type[1] ? (
+                  <>
+                    <p className={`${styles.vert_line}`}>|</p>
+                    <p className={`${styles.type}`}>{data.type[1]}</p>
+                  </>
+                ) : (
+                  ""
+                )}
+              </div>
+              <p className={`${styles.seller}`}>{profile.nickname}</p>
+            </div>
+          </div>
+          <div className={styles.card_line}></div>
+          <div className={styles.card_price_quantity}>
+            <div className={styles.card_price}>
+              <p className={`${styles.label}`}>가격</p>
+              <p className={`${styles.point}`}>{data.price}p</p>
+            </div>
+            <div className={styles.card_quantity}>
+              <p className={`${styles.label}`}>수량</p>
+              <p className={`${styles.value}`}>{data.quantity}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    ) : type === "내카드" ? (
       <>
         <div className={`${styles.img_wrap} ${exchangeImgClass}`}>
           <Image className={styles.img} src={data.image} fill alt="카드 이미지" priority />
@@ -33,8 +95,7 @@ export default function PhotoCard({ data, type = "판매카드", exchange }) {
                   ""
                 )}
               </div>
-              {/* 수정필요 */}
-              <p className={`${styles.seller} ${exchangeClass}`}>판매자</p>
+              <p className={`${styles.seller} ${exchangeClass}`}>{profile.nickname}</p>
             </div>
           </div>
           <div className={styles.card_line}></div>
--- components/MyShop/MyShopList.js
+++ components/MyShop/MyShopList.js
@@ -6,7 +6,7 @@ export default function MyShopList({ mySales = [] }) {
     <div className={styles.card_list}>
       {mySales.map((card, i) => (
         <div key={i}>
-          <PhotoCard data={card} type="내카드" />
+          <PhotoCard data={card} type="내판매카드" />
         </div>
       ))}
     </div>
