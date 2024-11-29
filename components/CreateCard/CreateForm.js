1cdfee23d519a773219512f2d33fbd8ae066cd40
Seo-Yurim <yurim2204@naver.com>
2024-11-29 09:27:45 +0900

    feat: 포토카드 생성 유효성 검사 훅 구현

--- components/CreateCard/CreateForm.js
+++ components/CreateCard/CreateForm.js
@@ -1,51 +1,91 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Input from "../Common/Input/Input";
 import styles from "./CreateForm.module.css";
 import Dropdown from "../Common/Input/Dropdown";
+import useValidate from "@/hooks/useValidate";
+import { createPhotoCard } from "@/lib/api/UserService";
 
 export default function CreateForm() {
-  const [name, setName] = useState("");
-  const [grade, setGrade] = useState("");
-  const [type, setType] = useState([]);
-  const [price, setPrice] = useState("");
-  const [totalQuantity, setTotalQuantity] = useState("");
-  const [image, setImage] = useState("");
-  const [description, setDescription] = useState("");
+  const router = useRouter();
+  const { values, setValues, errors, validate, handleChange } = useValidate({
+    name: "",
+    grade: "",
+    type: ["", ""],
+    price: "",
+    quantity: "",
+    image: "",
+    description: "",
+  });
+
+  const [error, setError] = useState("");
 
   const handleTypeChange = (index, value) => {
-    const newType = [...type];
+    const newType = [...values.type];
     newType[index] = value;
-    setType(newType);
+    handleChange({ target: { id: "type", value: newType } });
   };
 
-  const handleImageChange = (file) => {
-    setImage(file.name);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setValues({
+      ...values,
+      image: file,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validate()) {
+      return;
+    }
+
+    const card = {
+      name: values.name,
+      grade: values.grade,
+      type: [values.type[0], values.type[1]],
+      price: values.price,
+      quantity: values.quantity,
+      image: values.image,
+      description: values.description,
+    };
+
+    try {
+      await createPhotoCard(card);
+      return router.push("/pocketPlace");
+    } catch (err) {
+      console.error("상품 등록에 실패하였습니다.", err.message);
+      setError("상품 등록에 실패하였습니다.");
+    }
   };
 
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           label="포토카드 이름"
           type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={values.name}
+          onChange={handleChange}
           placeholder="포토카드 이름을 입력해 주세요"
         />
-        <Dropdown label="등급" name="grade" value={grade} setValue={setGrade} option="등급" />
+        <Dropdown
+          label="등급"
+          value={values.grade}
+          setValue={(value) => handleChange({ target: { id: "grade", value } })}
+          option="등급"
+        />
         <div className={styles.types}>
           <Dropdown
             label="속성 ①"
-            name="type"
-            value={type[0]}
+            value={values.type[0]}
             setValue={(value) => handleTypeChange(0, value)}
             option="속성"
           />
           <Dropdown
             label="속성 ②"
-            name="type"
-            value={type[1]}
+            value={values.type[1]}
             setValue={(value) => handleTypeChange(1, value)}
             option="속성"
           />
@@ -54,23 +94,23 @@ export default function CreateForm() {
           label="가격"
           type="text"
           name="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
+          value={values.price}
+          onChange={handleChange}
           placeholder="가격을 입력해 주세요"
         />
         <Input
           label="총 발행량"
           type="text"
           name="totalQuantity"
-          value={totalQuantity}
-          onChange={(e) => setTotalQuantity(e.target.value)}
+          value={values.totalQuantity}
+          onChange={handleChange}
           placeholder="총 발행량을 입력해 주세요"
         />
         <Input
           label="사진 업로드"
           type="file"
           name="image"
-          value={image}
+          value={values.image}
           onChange={handleImageChange}
           placeholder="사진 업로드"
         />
@@ -79,13 +119,16 @@ export default function CreateForm() {
             label="포토카드 설명"
             type="textarea"
             name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            value={values.description}
+            onChange={handleChange}
             placeholder="카드 설명을 입력해 주세요"
           />
         </div>
-        <button className={styles.create}>생성하기</button>
+        <button type="submit" className={styles.create}>
+          생성하기
+        </button>
       </form>
+      {error && <div>{error}</div>}
     </div>
   );
 }
