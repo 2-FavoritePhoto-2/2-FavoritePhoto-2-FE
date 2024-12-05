1ab1d1d627fe62f0ff3343c87228267ab5afc13d
bangsinchur <sinchurbang123@gmail.com>
2024-12-05 09:45:48 +0900

    상세페이지 이미지css 수정

--- pages/card/[id]/index.js
+++ pages/card/[id]/index.js
@@ -3,7 +3,7 @@ import QuantityCardBuyer from "@/components/Common/Quantity/QuantityCard_buyer";
 import QuantityCardSeller from "@/components/Common/Quantity/QuantityCard_seller";
 import PhotoCardExchange from "@/components/Common/PhotoCard/PhotoCardExchange";
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 import axios from "@/lib/api/CommonApi.js";
 import PhotoCard from "@/components/Common/PhotoCard/PhotoCard";
 import Exchange from "@/components/Common/Modal/Exchange";
@@ -197,8 +197,14 @@ export default function CardDetail({ data }) {
         <img src="/assets/icon_poketplace.png" className={styles.poketplace} />
         <div className={styles.title}>{card.name}</div>
         <div className={styles.card_details_table}>
-          <img src={card.image} className={styles.card_img} />
-          {isOwner ? <QuantityCardSeller data={data} /> : <QuantityCardBuyer data={data} />}
+          <div className={styles.image_table}>
+            <div>
+              <Image width={300} height={300} src={card.image} className={styles.card_img} />
+            </div>
+          </div>
+          <div>
+            {isOwner ? <QuantityCardSeller data={data} /> : <QuantityCardBuyer data={data} />}
+          </div>
         </div>
         <div className={styles.exchange_container}>
           {isOwner ? (
